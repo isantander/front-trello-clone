3c121543148b590d25edfae7af3efaf784dfab89
Ivan <isantander@gmail.com>
2024-12-22 17:58:30 -0300

    Column carga tareas desde el backend

--- src/components/trello/Column.jsx
+++ src/components/trello/Column.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Droppable } from '@hello-pangea/dnd';
 import TaskCard from './TaskCard';
-import { useTaskContext } from '../../context/TasksContext';
+import { useState, useEffect } from 'react';
 
 const Column = ({ estado }) => {
-  const { tasks } = useTaskContext();
+
+  const [tasks, setTasks] = useState([]); // Estado para almacenar las tareas
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:3000/tareas');
+      const data = await response.json();
+      setTasks(data.data); // Actualiza el estado con las tareas obtenidas
+    } catch (error) {
+      console.error('Error al obtener las tareas:', error);
+    }
+  };
+
+  // Llama a fetchTasks cuando el componente se monta
+  useEffect(() => {
+    fetchTasks();
+  }, []); // El segundo argumento [] asegura que esto se ejecute solo al montar
+
+
   const tasksFiltradas = tasks.filter((task) => task.estado === estado);
 
-  console.log(tasks);
+  console.log("tareaas" , tasks);
   return (
     <Droppable droppableId={estado}>
       {(provided) => (
